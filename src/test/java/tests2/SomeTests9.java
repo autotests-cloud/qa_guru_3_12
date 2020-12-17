cdc5ef53d4428985f6ff1cc294450c2d4b9ccfc7
svasenkov <aanher@gmail.com>
2020-12-17 20:29:45 +0300

    added tests

---
+++ src/test/java/tests2/SomeTests9.java
@@ -0,0 +1,27 @@
+package tests2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeTests9 {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+}
